7cc29457c47c3467196e7ec33b1b93faf5ee1be1
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-03-21 12:30:54 -0500

    move cronjob to icarus-crt11

--- Xporter/removeBottomCRTBackendFiles.sh
+++ Xporter/removeBottomCRTBackendFiles.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ssh icarus-crt11 "find /scratch_local/crt_tests/backend_data/ -path \"/scratch_local/crt_tests/backend_data/runs1/DATA/Run_*/binary/*\" -type f -mtime +7 -exec rm -f '{}' +"
+# 2024-03-21 MV: commenting out the ssh command, cronjobs don't pick up kerberos tickes easily so moving to running this directly on icarus_crt11
+#ssh icarus-crt11 "find /scratch_local/crt_tests/backend_data/ -path \"/scratch_local/crt_tests/backend_data/runs1/DATA/Run_*/binary/*\" -type f -mtime +7 -exec rm -f '{}' +"
+
+find /scratch_local/crt_tests/backend_data/ -path \"/scratch_local/crt_tests/backend_data/runs1/DATA/Run_*/binary/*\" -type f -mtime +7 -exec rm -f '{}' +
--- Xporter/update_crontabs.sh
+++ Xporter/update_crontabs.sh
@@ -14,10 +14,21 @@ done
 
 for server in icarus-evb06
 do
-    cat $1 logfile_crontab.ctab bottomcrt_crontab.ctab icarus_evb06_db.ctab > tmp.ctab
+    cat $1 logfile_crontab.ctab icarus_evb06_db.ctab > tmp.ctab
     echo "crontab to add:"
     cat tmp.ctab
     echo "Executing: ssh ${server} crontab ~icarus/FileTransfer/sbndaq-xporter/Xporter/tmp.ctab"
     ssh $server "crontab ~icarus/FileTransfer/sbndaq-xporter/Xporter/tmp.ctab"
     rm tmp.ctab
 done
+
+for server in icarus-crt11
+do
+    cat $1 bottomcrt_crontab.ctab > tmp.ctab
+    echo "crontab to add:"
+    cat tmp.ctab
+    echo "Executing: ssh ${server} crontab ~icarus/FileTransfer/sbndaq-xporter/Xporter/tmp.ctab"
+    ssh $server "crontab ~icarus/FileTransfer/sbndaq-xporter/Xporter/tmp.ctab"
+    rm tmp.ctab
+done
+
