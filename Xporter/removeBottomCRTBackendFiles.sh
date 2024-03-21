21a97bdc96993981fb303e5f6dac5cfdec5632b4
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-03-21 12:47:38 -0500

    remove escape characters

--- Xporter/removeBottomCRTBackendFiles.sh
+++ Xporter/removeBottomCRTBackendFiles.sh
@@ -6,4 +6,4 @@ echo "Running removeBottomCRTBackendFiles.sh at ${timestamp}"
 # 2024-03-21 MV: commenting out the ssh command, cronjobs don't pick up kerberos tickets easily so moving to running this directly on icarus_crt11
 #ssh icarus-crt11 "find /scratch_local/crt_tests/backend_data/ -path \"/scratch_local/crt_tests/backend_data/runs1/DATA/Run_*/binary/*\" -type f -mtime +7 -exec rm -f '{}' +"
 
-find /scratch_local/crt_tests/backend_data/ -path \"/scratch_local/crt_tests/backend_data/runs1/DATA/Run_*/binary/*\" -type f -mtime +7 -exec rm -f '{}' +
+find /scratch_local/crt_tests/backend_data/ -path "/scratch_local/crt_tests/backend_data/runs1/DATA/Run_*/binary/*" -type f -mtime +7 -exec rm -f '{}' +
