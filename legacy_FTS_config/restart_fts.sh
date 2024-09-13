69638f65b5526ad1ec279a06b8a294f0a6f75c23
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-09-13 11:20:56 -0500

    remove evb06 from running FTS

--- legacy_FTS_config/restart_fts.sh
+++ legacy_FTS_config/restart_fts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05 icarus-evb06
+for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05
 do
-    ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/FTS_config; source setup_fts.sh; stop_fts; start_fts'
+    ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/legacy_FTS_config; source setup_fts.sh; stop_fts; start_fts'
 done
 
--- legacy_FTS_config/start_fts.sh
+++ legacy_FTS_config/start_fts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05 icarus-evb06
+for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05
 do
     ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/FTS_config; source setup_fts.sh; start_fts'
 done
