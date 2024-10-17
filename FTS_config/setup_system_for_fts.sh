22552db03cd11885fc6c992d78260db00bbbbf1d
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-10-17 08:41:02 -0500

    enable-linger for icarusraw user

--- FTS_config/setup_system_for_fts.sh
+++ FTS_config/setup_system_for_fts.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
 # Things to do to setup for FTS
 #
-# Generally this follows from these webpages:
+# Generally this follows from these webpages
 # https://cdcvs.fnal.gov/redmine/projects/filetransferservice/wiki
-#
-# This should be run as user `icarusraw`
+# https://github.com/containers/podman/blob/main/docs/tutorials/rootless_tutorial.md
+
+# this should be run as user `icarusraw`
+if [ "$(whoami)" != "icarusraw" ]; then
+    echo "This script must be run as icarusraw, you silly goose!"
+    exit 1
+fi
+
+# enable linger mode for icarusraw: avoids container exiting after user logs out
+# https://github.com/containers/podman/blob/main/troubleshooting.md#21-a-rootless-container-running-in-detached-mode-is-closed-at-logout
+loginctl enable-linger $(whoami) 
 
 # setup some configuration for rootless containers
 CONFFILE="${HOME}/.config/containers/storage.conf" 
@@ -17,4 +28,5 @@ fi
 podman pull imageregistry.fnal.gov/sam-zerg/fermifts:latest
 
 # check listed images
+echo "List of available podman images:"
 podman images 
