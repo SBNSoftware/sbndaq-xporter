7ada22ccd8a4c6f292af42a4a6394c111b97d9a2
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-10-17 21:37:50 -0500

    update path to legacy scripts

--- FTS_config/legacy/start_fts.sh
+++ FTS_config/legacy/start_fts.sh
@@ -2,6 +2,6 @@
 
 for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05
 do
-    ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/FTS_config; source setup_fts.sh; start_fts'
+    ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/FTS_config/legacy; source setup_fts.sh; start_fts'
 done
 
