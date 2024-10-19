86cdde156f708c25c8cc93c604a86321dc9164f2
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-10-18 20:43:07 -0500

    full server list

--- FTS_config/start_fts.sh
+++ FTS_config/start_fts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05
+for server in icarus-evb01 icarus-evb02 icarus-evb03 icarus-evb04 icarus-evb05 icarus-evb06
 do
     ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/FTS_config; ./run_fts_container.sh'
 done
