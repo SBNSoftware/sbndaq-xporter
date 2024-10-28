de456c0047ad1dec69dd8c004885fa6261c3cd3b
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2024-10-28 08:52:14 -0500

    add comment

--- Xporter/removeOnMonFiles.sh
+++ Xporter/removeOnMonFiles.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# this needs to run on each evb machine as /data is on local disk
+# (technically only the one that runs the OM)
 find /data/onmon_files -name '*.root' -type f -mtime 0.25 -delete
