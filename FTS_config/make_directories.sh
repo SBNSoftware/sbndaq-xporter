56cbfb5b4962895a13439029a29671c111c37bb3
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2025-09-12 14:09:22 -0500

    add new evbs

--- FTS_config/make_directories.sh
+++ FTS_config/make_directories.sh
@@ -2,11 +2,12 @@
 # Directories to make 
 #
 
-hosts='icarus-evb12'
+hosts='icarus-evb08 icarus-evb09 icarus-evb10 icarus-evb12'
 for host in $hosts
 do
     echo "Making directories on $host"
     ssh icarus@$host 'mkdir -p /data/daq; chmod a+rwX /data/daq'
     ssh icarus@$host 'mkdir -p /data/test_daq; chmod a+rwX /data/test_daq'
+    ssh icarus@$host 'mkdir -p /data/onmon_files; chmod g+w,o+rX /data/onmon_files'
     ssh icarusraw@$host 'mkdir -p /data/fts_dropbox; chmod a+rwX /data/fts_dropbox'
 done
