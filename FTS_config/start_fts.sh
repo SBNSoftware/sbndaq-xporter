2534f22c37053a5e0ef57b93b146412c01102e69
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2025-10-02 21:29:04 -0500

    update after new evbs

--- FTS_config/start_fts.sh
+++ FTS_config/start_fts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for server in icarus-evb08 icarus-evb09 icarus-evb10 icarus-evb12
+for server in icarus-evb07 icarus-evb08 icarus-evb09 icarus-evb10 icarus-evb11 icarus-evb12
 do
     ssh icarusraw@$server 'cd ~icarusraw/sbndaq-xporter/FTS_config; ./run_fts_container.sh'
 done
