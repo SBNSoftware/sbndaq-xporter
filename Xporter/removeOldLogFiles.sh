60ded76676abe3eebf564604da2f0c581e74d15b
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2026-01-06 14:27:21 -0600

    update removal policies

--- Xporter/removeOldLogFiles.sh
+++ Xporter/removeOldLogFiles.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# remove any logfile older than 90 days
 find /daq/log -type f -mtime +90 -exec rm -f {} \;
+
+# remove broken symlinks
 find /daq/log -type l ! -exec test -e {} \; -exec rm {} \;
-find /daq/log -type d -empty -exec rmdir {} \;
+
+# remove empty directories
+find /daq/log -depth -type d -empty -exec rmdir {} \;
+
+# remove specific logfiles older than 14 days (metrics, triggerdb)
+# fts logs follow same policy, but different script run by root user
 find /daq/log/metrics/* -type f -mtime +14 -exec rm -f {} \;
+find /daq/scratch/log/* -type f -mtime +14 -exec rm -f {} \;
 find /daq/log/triggerdb/* -type f -mtime +14 -exec rm -f {} \;
-rm /daq/log/grafana/graphite/exception.log
-rm /daq/log/grafana/carbon/listener.log
