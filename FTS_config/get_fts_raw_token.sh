9f998bb10cafc2765c9ee2702a4f6daba61e0e3d
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2026-02-05 13:34:11 -0600

    make cronjob logging more robust

--- FTS_config/get_fts_raw_token.sh
+++ FTS_config/get_fts_raw_token.sh
@@ -3,10 +3,17 @@
 host=$(hostname | awk -F'.' '{print $1}')
 timestamp=`date +%Y_%m_%d`
 now=`date "+%Y-%m-%d %T"`
-logfile="/daq/log/fts_logs/${host}/icarusraw_token_${host}_${timestamp}.log"
+logfile="/daq/log/fts_logs/${host}/${USER}_token_${host}_${timestamp}.log"
 
-echo "$now : Obtaining icarusraw token on ${host}" >> ${logfile}
+echo "$now : Obtaining ${USER} token on ${host}" >> ${logfile}
 
-htgettoken -i icarus -a htvaultprod.fnal.gov --credkey=icarusraw/managedtokens/fifeutilgpvm01.fnal.gov -r raw --nooidc --nokerberos --nossh --minsecs 5400 >> ${logfile} 2>&1
+# specify token location (default)
+export BEARER_TOKEN_FILE=/run/user/$UID/bt_u$UID
+
+# get token
+htgettoken -v -i icarus -a htvaultprod.fnal.gov --credkey=${USER}/managedtokens/fifeutilgpvm01.fnal.gov -r raw --nooidc --nokerberos --nossh --minsecs 5400 >> ${logfile} 2>&1
+
+# check token is valid
+httokendecode -H >> ${logfile} 2>&1
 
 exit 0
